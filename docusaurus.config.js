1ef2e92d97f98a4f0a52eca2732a11c672734b3f
Mohammad Zulfikar <48849323+mohzulfikar@users.noreply.github.com>
2021-05-29 16:03:09 +0700

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,8 +7,8 @@ module.exports = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'sirius-400', // Usually your GitHub org/user name.
+  projectName: 'cloud-docs', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'Travel Partner by Sirius',
